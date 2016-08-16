1df001025ad8056bced2c8a79afe1458b5aef58b
charlieperson <charlie.person@hyperisland.se>
2016-08-16 01:03:53 -0500

    Improves challenge 6 solution by making use of filter instead of forEach

--- challenge6/extensionFilter.js
+++ challenge6/extensionFilter.js
@@ -3,11 +3,10 @@ var path = require('path');
 
 module.exports = function(folder, extension, callback){
   fs.readdir(folder, function(err, list) {
-    var array = [];
     if(err) return callback(err);
 
-    list.forEach(function(item) {
-      if(path.extname(item) === '.' + extension) array.push(item);
+    var array = list.filter(function(item) {
+      return(path.extname(item) === '.' + extension);
     });
     callback(null, array);
   });
