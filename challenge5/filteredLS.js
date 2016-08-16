b6d518558c6f6cbc0b7c53459e92048e081ce27f
charlieperson <charlie.person@hyperisland.se>
2016-08-15 19:22:08 -0500

    Completes challenge 5

---
+++ challenge5/filteredLS.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+var path = require('path');
+
+fs.readdir(process.argv[2], function(err, list){
+  list.forEach(function(item) {
+    if(path.extname(item) === '.' + process.argv[3]) console.log(item);
+  });
+});
