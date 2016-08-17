912eddb909c35bc563da5df486fbc119fb487156
charlieperson <charlie.person@hyperisland.se>
2016-08-17 13:46:27 -0500

    Adds error handeling to challenge 8

--- challenge8/httpCollect.js
+++ challenge8/httpCollect.js
@@ -3,6 +3,7 @@ var bl = require('bl');
 
 http.get(process.argv[2], function(resp) {
   resp.pipe(bl(function(err, data) {
+    if(err) return console.error(err);
     console.log(data.length);
     console.log(data.toString());
   }));
