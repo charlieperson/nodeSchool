7a22e088e72072a55a191b239ecfe0ee3f866907
charlieperson <charlie.person@hyperisland.se>
2016-08-17 22:33:24 -0500

    Refactors challenge 11 to immediately pipe the read stream from given file to http response

--- challenge11/httpFileServer.js
+++ challenge11/httpFileServer.js
@@ -2,11 +2,7 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function(request, response) {
-  var fileStream = fs.createReadStream(process.argv[3]);
-  fileStream.pipe(response);
-  // console.log(process.argv[3]);
-  // console.log('request', request);
-  // console.log('response', response);
+  fs.createReadStream(process.argv[3].pipe(response));
 });
 
 server.listen(process.argv[2]);
