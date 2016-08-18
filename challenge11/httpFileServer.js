2cb008fa1055bbbcaea843ae842257cfa4acd8c2
charlieperson <charlie.person@hyperisland.se>
2016-08-17 22:48:37 -0500

    Fixes parenthesis error on challenge 11 refactor

--- challenge11/httpFileServer.js
+++ challenge11/httpFileServer.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function(request, response) {
-  fs.createReadStream(process.argv[3].pipe(response));
+  fs.createReadStream(process.argv[3]).pipe(response);
 });
 
 server.listen(process.argv[2]);
